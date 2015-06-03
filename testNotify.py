27217ec88306faa775dd5a918b6f79497d95b1f6
thisisG <geir.istad.11@aberdeen.ac.uk>
2015-06-03 14:32:21 +0100

    Modified testNotify

--- testNotify.py
+++ testNotify.py
@@ -2,18 +2,49 @@ import sys
 import time
 from watchdog.observers import Observer
 
-if __name__ == "__main__":
-    path = '/sys/class/gpio/gpio30/value'
-    event_handler = eventHappened()
-    observer = Observer()
-    observer.schedule(event_handler, path, recursive=False)
-    observer.start()
-    try:
-        while True:
-            time.sleep(1)
-    except KeyboardInterrupt:
-        observer.stop()
-    observer.join()
+"""
+class MyHandler(FileSystemEventHandler):
+    """
+    # React to changes in YAML files, handling create, update, unlink
+    # explicitly. Ignore directories. Warning: does not handle move
+    # operations (mv f1.yaml f2.yaml) isn't handled.
+    """
+
+    def catch_all(self, event, op):
+
+        if event.is_directory:
+            return
+
+        filename = event.src_path
+        extension = os.path.splitext(filename)[-1].lower()
+        if extension == '.yaml':
+            print "YAML: (%s) %s" % (op, filename)
+            err = validyaml(filename)
+            if err is not None:
+                notify("%s\n\n%s" % (os.path.basename(filename), str(err)))
+                print "ERROR in loading yaml (%s)" % err
+
+    def on_created(self, event):
+        self.catch_all(event, 'NEW')
+
+    def on_modified(self, event):
+        self.catch_all(event, 'MOD')
+"""
 
 def eventHappened():
     print('event!')
+
+
+path = '/sys/class/gpio/gpio30/value'
+event_handler = eventHappened()
+observer = Observer()
+observer.schedule(event_handler, path, recursive=False)
+observer.start()
+try:
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
+    observer.stop()
+observer.join()
+
+
